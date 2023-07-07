1398697e526be3868769628545b352c287beff9b
rindra <andrijuvenal@gmail.com>
2023-07-07 13:44:21 +0200

    sp10
    sp10

--- src/java/etu1912/framework/fonction/Main.java
+++ src/java/etu1912/framework/fonction/Main.java
@@ -5,8 +5,12 @@
  */
 package etu1912.framework.fonction;
 import etu1912.framework.Mapping;
+import etu1912.framework.ModelView;
+import java.lang.reflect.Method;
 import java.util.HashMap;
 import java.util.Vector;
+import model.Employer;
+
 
 /**
  *
@@ -16,19 +20,54 @@ public class Main {
     public static void main(String[] args){
         Utilitaire u = new Utilitaire();
         HashMap<String, Mapping> mappingUrls = new HashMap<>();
-        
-        String path = "C:\\Users\\ITU\\Documents\\NetBeansProjects\\Sprint1\\build\\web\\WEB-INF\\classes";
-        Vector<String> str = u.getNomPackages(null,path,null);
+        /*Vector<String> str = u.getNomPackages(null,path,null);
         Vector<Class> cla = u.getClassViaPackage(str);
         u.allMapping(mappingUrls);
-        for(HashMap.Entry<String, Mapping>entry : mappingUrls.entrySet()){
-            System.out.println("okok " +entry.getKey() +" valeur " +entry.getValue().getCla() +" method "+entry.getValue().getMethod());
-        }
+      
+        try{
+           /* String slug = "ajout-emp";
+            
+            if(mappingUrls.containsKey(slug)){
+                Mapping map = mappingUrls.get(slug);
+                Class<?> clas = Class.forName(map.getCla());
+                //Class clas = Employer.class;
+                Method fonction = clas.getDeclaredMethod(map.getMethod());
+                //fonction.setAccessible(false);
+                ModelView page = (ModelView) fonction.invoke(clas.newInstance());
+                System.out.println("okok : "+page.getView());
+            }*/
+          /* Employer e = new Employer();
+           ModelView mod = e.ajouterEmployer();
+           Employer[] em = new Employer[4];
+           HashMap<String,Object> l = mod.getListe();
+           for(HashMap.Entry<String, Object>entry : l.entrySet()){
+               em = (Employer[]) entry.getValue();
+               System.out.println(em[0].getNom());
+               System.out.println(entry.getKey());
+           }
+        }catch(Exception e){
+            e.printStackTrace();
+        }*/
+        /*for(HashMap.Entry<String, Mapping>entry : mappingUrls.entrySet()){
+            try {
+                        Class clas = Class.forName(entry.getValue().getCla());
+                        
+                        System.out.println("io ny valiny : "+clas.getSimpleName());
+                       
+        
+                        String page = String.valueOf(clas.getDeclaredMethod("ajouterEmployer").invoke(clas));
+                        System.out.println("okok : "+page);
+                        //System.out.println(page.getView()); 
+                    } catch (Exception ex) {
+                       ex.printStackTrace();
+                    }
+        }*/
+        
         /*for(int i=0;i<str.size();i++){
             System.out.println(str.get(i));
         }*/
-        for(int i=0;i<cla.size();i++){
+        /*for(int i=0;i<cla.size();i++){
             System.out.println(cla.get(i).getSimpleName());
-        }
+        }*/
     }
 }
---
+++ web/Affichage/index.jsp
@@ -0,0 +1,24 @@
+<%-- 
+    Document   : index
+    Created on : 18 avr. 2023, 11:27:10
+    Author     : ITU
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Welcome to our FrameWork!</h1>
+        <form action="Sprint1/verification">
+            <p>Nom :</p>
+            <input type="text" name="nom">
+            <p>Mot de passe :</p>
+            <input type="text" name="mdp">
+            <input type="submit" value="Envoyer">   
+        </form>
+    </body>
+</html>
